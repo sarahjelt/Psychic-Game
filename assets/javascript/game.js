bb60ac988df7ca463fe3937569cebba19f4b8ea8
Sara Hjelt <sarahjelt@Saras-MacBook-Air.local>
2017-09-03 18:25:14 -0400

    got the guesses to reset yay

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var win = 0;
 var loss = 0;
 var guessesLeft = 9;
 
-var userGuess = [];
+var userPick = [];
 
 //array of all the guesses the computer could possibly make
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -13,11 +13,15 @@ render ();
 
 // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
-      // Determines which key was pressed.
+
+    // Determines which key was pressed.
     var userGuess = event.key;
+    guessArray(userGuess);
+
     console.log(userGuess);
-      // Randomly chooses a choice from the options array. This is the Computer's guess.
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
     console.log(computerGuess);
 
 if (guessesLeft > 0) {
@@ -28,21 +32,29 @@ if (guessesLeft > 0) {
     (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") ||
     (userGuess === "y") || (userGuess === "z")) {
 
-    	if (userGuess === computerGuess) {
+    if (userGuess === computerGuess) {
         win++;
         guessesLeft = 9;
+        userPick.splice(0,10);
         } else {
         guessesLeft--;
         }
       }
     }
 
-	//reset counter and tally loss
-	if (guessesLeft === 0) {
-		loss++;
-		guessesLeft = 9;
-	} 
+//reset counter and tally loss, clear past guesses
+if (guessesLeft === 0) {
+	loss++;
+	guessesLeft = 9;
+	userPick.splice(0,10);
+} 
 
+//PSEUDOCODE 
+//if event.key is not part of computerGuess array (e.g., number, enter key, tab etc.), do not print to guessArray
+
+//do not print duplicate values to past guesses, do not decrement guessesLeft
+
+//also, want to remove guesses whenever guessesLeft resets to 0!!!
 
     console.log(guessesLeft);
 
@@ -50,10 +62,13 @@ if (guessesLeft > 0) {
     };
 
 
-    function render () {
+function render () {
         document.getElementById("win").innerHTML = win;
         document.getElementById("loss").innerHTML = loss;
         document.getElementById("guessesLeft").innerHTML = guessesLeft;
-        document.getElementById("userGuess").innerHTML = userGuess;
+        document.getElementById("userGuess").innerHTML = userPick;
     }
 
+function guessArray (val1) {
+	userPick.push(val1);
+}
