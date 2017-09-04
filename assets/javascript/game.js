ca4d527e2cecd116833135f6bfb4bf7738d7522e
Sara Hjelt <sarahjelt@Saras-MacBook-Air.local>
2017-09-04 12:13:39 -0400

    psychic game is in good working order

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var userPick = [];
 
 //array of all the guesses the computer could possibly make
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var userChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var userOptions = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 render ();
 
@@ -21,16 +21,11 @@ render ();
     console.log(userGuess);
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
     console.log(computerGuess);
 
 if (guessesLeft > 0) {
     // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") ||
-    (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") ||
-    (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") ||
-    (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") ||
-    (userGuess === "y") || (userGuess === "z")) {
+    if (userOptions.includes(userGuess) === true) {
 
     if (userGuess === computerGuess) {
         win++;
@@ -42,6 +37,12 @@ if (guessesLeft > 0) {
       }
     }
 
+//alert if user presses non A–Z key
+    if (userOptions.includes(userGuess) === false) {
+      alert("Please pick a valid A–Z key");
+      userPick.pop();
+    }
+
 //reset counter and tally loss, clear past guesses
 if (guessesLeft === 0) {
 	loss++;
@@ -49,12 +50,11 @@ if (guessesLeft === 0) {
 	userPick.splice(0,10);
 } 
 
+
 //PSEUDOCODE 
-//if event.key is not part of computerGuess array (e.g., number, enter key, tab etc.), do not print to guessArray
 
 //do not print duplicate values to past guesses, do not decrement guessesLeft
 
-//also, want to remove guesses whenever guessesLeft resets to 0!!!
 
     console.log(guessesLeft);
 
@@ -67,8 +67,15 @@ function render () {
         document.getElementById("loss").innerHTML = loss;
         document.getElementById("guessesLeft").innerHTML = guessesLeft;
         document.getElementById("userGuess").innerHTML = userPick;
-    }
+    };
 
 function guessArray (val1) {
+	if (userPick.includes(val1) === true) {
+        alert("You've already guessed that one!");
+        guessesLeft++;
+    } else if (userOptions.includes(val1) === true) {
 	userPick.push(val1);
-}
+	}
+  };
+
+
